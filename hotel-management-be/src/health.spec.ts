878f65e236795384e72d2da077f08cba04abe1f3
nghiahoang14 <nghiahoang814@gmail.com>
2026-06-20 19:33:07 +0700

    test case 3: unit test fail

--- hotel-management-be/src/health.spec.ts
+++ hotel-management-be/src/health.spec.ts
@@ -4,6 +4,6 @@ describe('health check', () => {
   it('should return true', () => {
     expect(true).toBe(true);
     // test case 3:  unit test fail
-    //  throw new Error("Demo unit test fail");
+     throw new Error("Demo unit test fail");
   });
 });
